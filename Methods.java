9e4978079238d3ccfcf8471b96e4fa338dc949bc
thepradeep <thebaskota@gmail.com>
2019-12-05 21:17:13 +0545

    Changes to be committed: 	modified:   Methods.java

--- Methods.java
+++ Methods.java
@@ -1,9 +1,17 @@
 package com.thebaskota;
 
 public class Methods {
-
+    public static int calculateSum( int num1, int num2)
+    {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 3, b = 5, result;
+		result = calculateSum(a,b);
+		System.out.println("the sum of two numbers is " + result);
 
 	}
 
